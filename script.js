37c911d04c9c27f8a82bb306dca29bab00a441a2
PabloPiliavsky <pablopiliavsky21@gmail.com>
2023-06-04 19:14:22 -0300

    agregue las funciones actualizarTabla y cagrarApi para poder mostrar los datos en la tabla utilizando ellas y la funcion reiniciarTabla

--- script.js
+++ script.js
@@ -17,6 +17,33 @@ function iniciarPagina() {
         cuerpoTabla.innerHTML = "";
     }
 
+    async function cargarApi() {
+        try {
+            let res = await fetch(url)
+            let json = await res.json();
+            console.log(json);
+            grupos= json;
+            console.log(grupos);
+            actualizarTabla(grupos);
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }
+
+
+    function actualizarTabla(arreglo) {
+        reiniciarTabla();
+        for (let i = 0; i < arreglo.length; i++) {
+            cuerpoTabla.innerHTML +=
+                "<tr> <td>" + arreglo[i].integrante1 + "</td>" +//cambio los clase precargar... y escribirlos asi?
+                "<td>" + arreglo[i].integrante2 + "</td>" +
+                "<td>" + arreglo[i].integrante3 + "</td>" +
+                "<td>" + arreglo[i].integrante4 + "</td>" + //preguntar por estos numeros que no toman en el precargar
+                "<td>" + arreglo[i].integrante5 + "</td> </tr>"
+        }
+    }
+
 
 
 
