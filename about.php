916e1211118a52265f06d593f282c33bcf146ab8
Joana Bugarin <Wooli3000@gmail.com>
2023-06-07 22:21:42 -0300

    se agregan los estilos de about.php a la hoja de estilos y se la vincula

--- about.php
+++ about.php
@@ -1,61 +1,8 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <link rel="stylesheet" href="estilos.css">
     <title>About</title>
-    <style>
-        /* Estilos CSS para la página */
-        body {
-            background-color: #f2f2f2;
-            font-family: Arial, sans-serif;
-            margin: 0;
-            padding: 20px;
-        }
-
-        h1 {
-            color: #333333;
-            text-align: center;
-        }
-
-        .container {
-            display: flex;
-            flex-wrap: wrap;
-            justify-content: center;
-            margin-top: 20px;
-        }
-
-        .card {
-            background-color: #ffffff;
-            border-radius: 8px;
-            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-            margin: 10px;
-            padding: 20px;
-            text-align: center;
-            width: 200px;
-            transition: transform 0.3s ease;
-        }
-
-        .card:hover {
-            transform: translateY(-10px);
-        }
-
-        .card img {
-            border-radius: 50%;
-            height: 100px;
-            margin-bottom: 10px;
-            width: 100px;
-        }
-
-        .card h2 {
-            color: #333333;
-            font-size: 18px;
-            margin-bottom: 5px;
-        }
-
-        .card p {
-            color: #666666;
-            font-size: 14px;
-        }
-    </style>
 </head>
 <body>
     <h1>About</h1>
