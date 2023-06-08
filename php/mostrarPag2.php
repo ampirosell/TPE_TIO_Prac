18c457db62377958291342800e885098552cce00
fer-gif <ferminhernando00@gmail.com>
2023-06-08 12:21:43 -0300

    mejoras en el route

--- php/mostrarPag2.php
+++ php/mostrarPag2.php
@@ -7,7 +7,7 @@ function imprimirHeader($titulo, $home,$about,$grupos){
         <meta http-equiv='X-UA-Compatible' content='IE=edge'>
         <title>{$titulo}</title>
         <meta name='viewport' content='width=device-width, initial-scale=1'>
-        <link rel='stylesheet' href='style.css' type='text/css'>
+        <link rel='stylesheet' href='../estilos.css' type='text/css'>
         <script type='text/javascript' src='script.js'></script>
     </head>
     <body>
--- route.php
+++ route.php
@@ -1,35 +1,34 @@
 <?php
 
-
 define("BASE_URL", 'http://' . $_SERVER['SERVER_NAME'] . dirname($_SERVER['PHP_SELF']));
 
 
-if (!empty($_REQUEST['action'])){
-    $accion = $_REQUEST['action'];
-} else{$accion = 'home';}
+require_once 'php/mostrarPag2.php';
+require_once 'php/paginas_mP2.php';
+
+
+
+if (!empty($_GET['action'])){
+    $action = $_GET['action'];
+} else{$action = 'home';}
     
 
-$params = explode("/", $accion);
+$params = explode("/", $action);
 
     switch ($params[0]) {
         case 'home':
-    
-            break;
-        case 'pagina2':
-           
-          
-            break;
-       
-        case 'pagina3':
-           
-            
-            break;
-        case 'pagina4':
-            
-            break;
-
+          inicio($params[0]);
+        break;
+        case 'about':
+            echo ("about");
+        break;
+        case 'grupo':
+            grupos();
+        break;
         default:
-            echo "No existe la pagina";
+            echo('No existe la pagina');
+        break;
+
         
     }
 ?>
